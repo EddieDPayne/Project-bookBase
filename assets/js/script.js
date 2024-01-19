08ba8f36f715f4584e72af3be6b8ace9db7ecb54
Eddie Payne <eddie_p89@hotmail.com>
2024-01-19 20:26:10 +1100

    added Open Library API fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,124 @@
-var searchButton = document.querySelector('#book-call')
-var inputSearcher = document.querySelector('#book-search')
+
+// VARIABLES /////////////////////////////////////////////////
+
+var input = document.querySelector('input');
+var button = document.querySelector('button');
+var booksList = document.getElementById('display-book-list');
+
 var googleKey = 'AIzaSyDDK7ZVkv0izkL1bXrc2SrnVlid_RDm9yM'
-var bookName = inputSearcher.value
 
-var requestUrlGoogle = `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookName}&key=${googleKey}`
-'https://www.googleapis.com/books/v1/volumes?q=intitle:harry&key=AIzaSyDDK7ZVkv0izkL1bXrc2SrnVlid_RDm9yM'
 
-searchButton.addEventListener('click', function () {
+///  CREATING AND GRABBING HTML ELEMENTS AND LINKING THEM TO VARIABLES ////////////////////////////////////
 
+var createBookList = function(book) {        
+var li = document.createElement('li');        
+var div = document.createElement('div');
+var singleBookRow = document.createElement('div');        
+var bookTitle = document.createElement('h2');
+var bookAuthors = document.createElement('h3');
+var bookDescription = document.createElement('p');
+var bookThumbnail = document.createElement('img');      
+
+/// INSERTING THE FETCHED BOOK DATA (e.g. title, author) and putting that info into the variables, which goes into the linked the HTML document
+
+      bookTitle.textContent = book.volumeInfo.title;
+      bookAuthors.textContent = book.volumeInfo.authors;
+      bookDescription.textContent = book.volumeInfo.description ? book.volumeInfo.description: " Description unavailable";
+      bookThumbnail.src = book.volumeInfo.imageLinks ? 
+      book.volumeInfo.imageLinks.thumbnail : 
+      
+
+ ///// below attaches the result data (book title, author, description, img) to the created div 
+ // and list html elements          
+      div.appendChild(bookTitle);
+      div.appendChild(bookAuthors);
+      div.appendChild(bookDescription);
+      singleBookRow.appendChild(bookThumbnail);
+      singleBookRow.appendChild(div);
+      li.appendChild(singleBookRow);
+
+      li.classList.add('list-group-item');
+      singleBookRow.classList.add('row');
+      div.classList.add('columns-large-4');
+      bookThumbnail.classList.add('columns-small-3', 'rwd');
+          
+      booksList.appendChild(li);
+  }
+
+// FETCH REQUEST FOR BOOK INFORMATION /////////////////////////////////////////////////
+
+var fetchRequest = function() {
+      if(input.value.length === 0) {
+  /////////// alert if user tries to search without entering a search word, i.e. text area is empty
+          alert('Please enter a search term');
+////////////////////////////////                                          ////////////////////////////////
+//////////////////   above is an alert and needs to be changed to a modal somehow  ////////////////////////////////
+////////////////////////////////                                         ////////////////////////////////
+      } else {
+          booksList.innerHTML = "";
+          var searchBook = input.value;
+          var searchURL = `https://www.googleapis.com/books/v1/volumes?q=${searchBook}&key=${googleKey}&printType=books`; 
+          
+          fetch(searchURL)
+          .then(function(response) {
+              return response.json();
+          })
+          .then(function(response2) {
+              return response2.items.map(function(book) {
+                createBookList(book)
+              })   
+
+            })  
+          .catch( function()  {
+              console.log(`There was an error`);       
+              //////// possibly insert an error message for user here using modals  ////////////////////////////////
+          }); 
+      }                 
     
+  }
 
-    fetch(requestUrlGoogle)
-    .then(function (response) {
-    return response.json();
-    })
-    .then(function (data) {
-    console.log(data)
-    displayTitle(data);
-    })
+  button.addEventListener("click", function() {
+    fetchRequest();
+  });
 
-    
-})
+/// SEARCH HISTORY / LOCAL STORAGE //////////////////////////////////////////////////////
 
-function displayTitle(data) {
-    if (data) {
 
-    } else {
-        "No books"
-    }
-    console.log("Data:", data);
+// Sets the book search term in localStorage
+var bookName = localStorage.getItem('searchBookStore');
 
-    var bookContainer = $('#display-book-list');
-
-    bookContainer.empty();
-
-    // refer the below line to object fetched from the API
-    var mainTitle = data.items[0].volumeInfo.title
-    console.log(data.items);
-    console.log(data.items[0]);
-    console.log(data.items[0].volumeInfo);
-    console.log(mainTitle);
-
-    var mainPublisher = $('<li>').text(`Author: ${data.items[0].volumeInfo.publisher}`);
-
-    bookContainer.append(mainTitle, mainPublisher);
+// Sets the input value in localStorage
+function recordBookData() {
+  localStorage.setItem('searchBookStore', input.value);
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////
+// Appends the search input from localStorage to the book list
+for (var i = 0; i < localStorage.length; i++) {
+  $(".saved-books").append("<p>" + localStorage.getItem(localStorage.key(i)) + "</p>");
+}
 
-// Nav functionality
+  button.addEventListener("click", recordBookData, function() {
+    fetchRequest();
+  });
 
-var elem = new Foundation.DropdownMenu(element, options);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//Open Library API 
+
+var openLibraryAPI = `http://openlibrary.org/search.json?q=${encodeURIComponent(book)}`;
+
+fetch(searchURL)
+.then(function(response) {
+    return response.json();
+})
+.then(function(response2) {
+    return response2.items.map(function() {
+      
+    })   
+
+  })  
+.catch( function()  {
+    console.log(`There was an error`);       
+    //////// possibly insert an error message for user here using modals  ////////////////////////////////
+}); 
