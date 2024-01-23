672c68c73b0e51c91f05b964abf91978c8f409ad
Troy <tydamon@hotmail.com>
2024-01-23 21:48:18 +1100

    debugged model model now shows up if search bar is empty when user tries to search

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 // VARIABLES 
 
 var input = document.querySelector('input');
-var button = document.querySelector('#button');
+var button = document.querySelector('#google-search-button');
+var m = document.getElementById("myModal");
 // var checkAvailability = document.querySelector('input[type="button" i]');
 var booksList = document.getElementById('display-book-list');    /// Google Books API generates into here
 // variable is of local storage in either state; containing history, or empty:
@@ -18,7 +19,9 @@ var googleKey = 'AIzaSyDDK7ZVkv0izkL1bXrc2SrnVlid_RDm9yM'
 
 var fetchRequest = function() {
   //--- modal: if the search bar is empty ----
-  if(input.value.length === 0) {
+  if(input.value.length === 0) {  
+    m.foundation('open');
+
        $(function() {
          modal.style.display = "block";
        });
@@ -120,9 +123,12 @@ var createBookList = function (book, openLibraryInfo) {
 
 // FETCH REQUEST FOR BOOK INFORMATION 
 
-var fetchRequest = function () {
-    if (input.value.length === 0) {
-        alert('Please enter a search term');
+var fetchRequest = function() {
+    //--- modal: if the search bar is empty ----
+  if(input.value.length === 0) {
+    $(function() {
+      modal.style.display = "block";
+    });
     } else {
         booksList.innerHTML = "";
         var searchBook = input.value;
@@ -172,7 +178,28 @@ function deleteSearchHistory() {
   searchHist.innerHTML = "";
 }
 
+// MODEL ---------------------------------------------------------------------------------
 
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("google-search-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
@@ -210,6 +237,8 @@ document.getElementById('delete').addEventListener("click", function () {
   searchOpenLibrary(bookTitle, isbnData);
   console.log(bookTitle)
   console.log(isbnData);
+
+  
 
 
 //-------------------------------------------------------------------------------------------
